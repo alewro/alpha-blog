c823471d9a1e25933e7c7cf823d88dc6a0cb6824
Aleksandra <aleksandra@wroblewscy.eu>
2023-02-13 16:14:41 +0100

    Add authentication gem and create a migration for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+#  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #'/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/'
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
