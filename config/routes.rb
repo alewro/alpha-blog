4babbbbbdab54b267ac4faae8ad3ca17569a26e9
Aleksandra <aleksandra@wroblewscy.eu>
2023-02-14 15:46:18 +0100

    Add form for sign up and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root 'page#home'
   get 'about', to: 'page#about'
   resources :articles
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
