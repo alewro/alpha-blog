3d8f2938be58dfabc9975cf4dff8864aa5aca9d8
Aleksandra <aleksandra@wroblewscy.eu>
2023-02-22 11:59:56 +0100

    Security updates for editing and deleting articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
