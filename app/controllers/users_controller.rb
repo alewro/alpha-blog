9fb7c3846d9c292fddbc61ae1315537b8514a254
Aleksandra <aleksandra@wroblewscy.eu>
2023-02-22 10:39:53 +0100

    Add current user session in application helper. Clean up users controller code. Add links to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -14,11 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was succesfully updated"
       redirect_to @user
@@ -30,17 +28,20 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have succesfylly sign up"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have succesfully sign up"
       redirect_to articles_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-
-
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
