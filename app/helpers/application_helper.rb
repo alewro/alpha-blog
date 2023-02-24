a77ea5f0253eeb5e969f533e1cddb1d972f50205
Aleksandra <aleksandra@wroblewscy.eu>
2023-02-24 14:43:43 +0100

    Set flash notice to green and flash alert to red

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 
+  def flash_class(level)
+    case level
+    when :notice then 'alert alert-success'
+    when :alert then 'alert alert-danger'
+    end
+  end
 end
