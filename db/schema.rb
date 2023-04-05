a73d836c8ac726140e6590ad758fa3f4997ef236
Aleksandra <aleksandra@wroblewscy.eu>
2023-04-05 12:04:23 +0200

    Category validation with unit tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_130353) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_093411) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
