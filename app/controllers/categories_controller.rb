d601c9447c9b782127b5402963af0570006a1df8
Aleksandra <aleksandra@wroblewscy.eu>
2023-04-14 10:23:48 +0200

    Add edit view for categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,20 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def category_params
